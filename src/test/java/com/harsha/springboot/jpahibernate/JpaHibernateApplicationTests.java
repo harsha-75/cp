b853f8199e766be6b352360c4f6e93b3d353d43b
harsha vardhan <harshavardhanmanne410@gmail.com>
2023-09-27 23:54:57 +0530

    ini

---
+++ src/test/java/com/harsha/springboot/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.harsha.springboot.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
